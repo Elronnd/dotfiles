d3809dde4a381037a0212769e91595d260504578
Moonchild <elronnd@elronnd.net>
2020-03-08 18:01:08 -0700

    stuff

---
+++ emacs_early-init.el
@@ -0,0 +1,10 @@
+(fringe-mode 0)
+(scroll-bar-mode 0)
+
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+(setq initial-scratch-message nil)
+
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-12"))
+(custom-set-faces '(default ((t (:foreground "#ffffff" :background "#050505")))))
